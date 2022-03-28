b5bc8fcb38b856afa774048fc3be8f8231f11e6e
Nirlley Costa <nirlleycosta@github.com>
2022-03-28 00:04:42 -0300

    findById implementation

--- src/db/DB.java
+++ src/db/DB.java
@@ -2,9 +2,7 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 /*
@@ -54,5 +52,25 @@ public class DB {
             throw new DbException(e.getMessage());
         }
 
+    }
+
+    public static void closeStatement(PreparedStatement st) {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
     }
 }
