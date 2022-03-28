bc6d453e8cd4e55052e697e21da0f00792de85c0
Nirlley Costa <nirlleycosta@gmail.com>
2022-03-27 23:08:03 -0300

    Seller class created

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,16 +2,22 @@ package application;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.Connection;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
+        //testing connection and classes from model.entities
         Connection conn = DB.getConnection();
 
         Department obj = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
         System.out.println(obj);
+        System.out.println(seller);
 
         DB.closeConnection();
     }
