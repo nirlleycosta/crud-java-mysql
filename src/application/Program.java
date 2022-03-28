c594a3736190ca30862965063f81257dfeb9bfa9
Nirlley Costa <nirlleycosta@gmail.com>
2022-03-28 00:04:42 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,13 +15,19 @@ public class Program {
         //testing connection and classes from model.entities
         Connection conn = DB.getConnection();
 
-        Department obj = new Department(1, "Books");
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
+
+       /* Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(obj);
-        System.out.println(seller);
+        System.out.println(seller);*/
 
         DB.closeConnection();
     }
