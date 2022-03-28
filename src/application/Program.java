73b08bbfa80dee87ae67b38f3eaf3901e69165ad
Nirlley Costa <nirlleycosta@gmail.com>
2022-03-27 22:57:46 -0300

    Department class created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import db.DB;
+import model.entities.Department;
 
 import java.sql.Connection;
 
@@ -8,6 +9,10 @@ public class Program {
     public static void main(String[] args) {
 
         Connection conn = DB.getConnection();
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+
         DB.closeConnection();
     }
 }
