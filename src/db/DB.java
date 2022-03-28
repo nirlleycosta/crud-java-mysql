df23cc971bff87a4d0a1f19c80b83b8dc386b2c2
Nirlley Costa <nirlleycosta@gmail.com>
2022-03-27 22:28:35 -0300

    DB created

---
+++ src/db/DB.java
@@ -0,0 +1,58 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/*
+ * Notes
+ * Why did I take DbException instead of SQLException?
+ * DbException is an extended class from RunTimeException (superclass) this class does not require exceptions handlers
+ * everytime. (try/catch) making it easier to code, you choose when you're going to use it.
+ * SQLException extends from Exception and this superclass always require exceptions handlers when coding.
+ * Plus, now you have a custom exception.
+ *
+ * */
+
+public class DB {
+
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Properties props = loadProperties();
+                String url = props.getProperty("dburl");
+                conn = DriverManager.getConnection(url, props);
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    public static void closeConnection() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    private static Properties loadProperties() {
+        try (FileInputStream fs = new FileInputStream("db.properties")) {
+            Properties props = new Properties();
+            props.load(fs);
+            return props;
+        }
+        catch (IOException e) {
+            throw new DbException(e.getMessage());
+        }
+
+    }
+}
