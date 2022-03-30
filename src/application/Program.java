c2779132a655d4d34f40a5ba015a11f8ad0af1a7
Nirlley Costa <nirlleycosta@gmail.com>
2022-03-30 15:35:22 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -45,7 +45,12 @@ public class Program {
 
         }
 
+        System.out.println("=== TEST 4: seller insert ====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
 
         DB.closeConnection();
     }
 }
+
