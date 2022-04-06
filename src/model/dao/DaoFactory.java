cc6263efd7970de3e689af7f9c2b3605eee532ef
Nirlley Costa <nirlleycosta@gmail.com>
2022-04-06 14:03:24 -0300

    createDepartmentDao in DaoFactory

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,18 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
+    public static DepartmentDao createDepartmentDao() {
+        return new DepartmentDaoJDBC(DB.getConnection());
+
+    }
+
     public static SellerDao createSellerDao() {
         return new SellerDaoJDBC(DB.getConnection());
     }
+
 }
