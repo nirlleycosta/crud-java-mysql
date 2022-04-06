00e0e63c1c761bc5da0f7bf85a52892372e17ec2
Nirlley Costa <nirlleycosta@gmail.com>
2022-04-06 14:05:25 -0300

    Seller entity test

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,58 +14,44 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
 
-        /*testing connection and classes from model.entities
-        Connection conn = DB.getConnection();
-       Department obj = new Department(1, "Books");
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println(obj);
-        System.out.println(seller);*/
-
-        SellerDao sellerDao = DaoFactory.createSellerDao();
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("=== TEST 1: seller findById ====");
+        System.out.println("=== TEST 1: seller findById =====");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
-        System.out.println("=== TEST 2: seller findByDepartment ====");
+        System.out.println("\n=== TEST 2: seller findByDepartment =====");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
         for (Seller obj : list) {
-            //Para cada objeto tipo Seller na minha List list, eu vou mandar imprimir o objeto tipo Seller
             System.out.println(obj);
-
         }
-        System.out.println("=== TEST 3: seller findByAll ====");
+
+        System.out.println("\n=== TEST 3: seller findAll =====");
         list = sellerDao.findAll();
         for (Seller obj : list) {
-            //Para cada objeto tipo Seller na minha List list, eu vou mandar imprimir o objeto tipo Seller
             System.out.println(obj);
-
         }
 
-        System.out.println("=== TEST 4: seller insert ====");
+        System.out.println("\n=== TEST 4: seller insert =====");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
-
-        System.out.println("=== TEST 5: seller update ====");
+        System.out.println("\n=== TEST 5: seller update =====");
         seller = sellerDao.findById(1);
-        seller.setName("Martha Updated");
+        seller.setName("Martha Waine");
         sellerDao.update(seller);
-        System.out.println("Update completed" + seller);
+        System.out.println("Update completed");
 
-        System.out.println("=== TEST 6: seller update ====");
-        System.out.println("Enter the id for delete test");
-        int id = scanner.nextInt();
+        System.out.println("\n=== TEST 6: seller delete =====");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
         System.out.println("Delete completed");
 
-
-        DB.closeConnection();
+        sc.close();
     }
 }
-
