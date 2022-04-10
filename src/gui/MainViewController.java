fbc3bdb1b29dd47056a3540727dbbc1b763688bf
Nirlley Costa <nirlleycosta@gmail.com>
2022-04-09 23:31:07 -0300

    DepartmentList controller

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -45,7 +45,7 @@ public class MainViewController implements Initializable {
     }
 
     @Override
-    public void initialize(URL uri, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb) {
 
     }
 
